f46138dd4c2657d96b476fdb60098bd77231f7f8
josefroslan <josefroslan@gmail.com>
2023-04-22 02:45:43 +0800

    portfolio update. added node.js and react.js logo.

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ const projectLinks = document.querySelectorAll('.project a');
 // Add an event listener to each anchor tag
 projectLinks.forEach(link => {
     link.addEventListener('click', event => {
-        // event.preventDefault(); // Prevent the default behavior of the link
         link.classList.add('clicked'); // Add a class to the clicked anchor tag
     });
 });
